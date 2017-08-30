21911211be89fe4728dd7520afdcc88288b06119
KotoBloodwell <julianaloaiza27@gmail.com>
2017-08-30 00:15:10 -0500

    ..

--- app/src/main/java/koto/marvel/Api/Marvel.java
+++ app/src/main/java/koto/marvel/Api/Marvel.java
@@ -15,7 +15,18 @@ import retrofit2.http.Path;
 
 public interface Marvel {
 
-    @GET("v1/public/comics/{seriesId}/characters?ts=1&apikey=28697e469dba62131372558a9436fdb7&hash=f857365d96b5565d485a98abfc542d17")
+    String BASE_URL = "https://gateway.marvel.com:443/";
+
+    String API_KEY_KEY = "apikey";
+    String API_KEY_VALUE = "28697e469dba62131372558a9436fdb7";
+
+    String TIME_STAMP_KEY = "ts";
+    String TIME_STAMP_VALUE = "1";
+
+    String HASH_KEY = "hash";
+    String HASH_VALUE = "f857365d96b5565d485a98abfc542d17";
+
+    @GET("v1/public/comics/{seriesId}/characters")
     Call <Basic<Data<ArrayList<SuperHero>>>> getSuperHeroes(@Path("seriesId") int seriesId);
 
 }
--- app/src/main/java/koto/marvel/MainActivity.java
+++ app/src/main/java/koto/marvel/MainActivity.java
@@ -2,12 +2,41 @@ package koto.marvel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import koto.marvel.Api.Marvel;
+import koto.marvel.Api.MarvelService;
+import koto.marvel.Models.Basic;
+import koto.marvel.Models.Data;
+import koto.marvel.Models.SuperHero;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int ID_SERIE = 354;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Call<Basic<Data<ArrayList<SuperHero>>>> callSuperHero = MarvelService.getMarvelApi().getSuperHeroes(ID_SERIE);
+
+        callSuperHero.enqueue(new Callback<Basic<Data<ArrayList<SuperHero>>>>() {
+            @Override
+            public void onResponse(Call<Basic<Data<ArrayList<SuperHero>>>> call, Response<Basic<Data<ArrayList<SuperHero>>>> response) {
+                Toast.makeText(MainActivity.this, response.body().getData().getResults().get(0).getName(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Basic<Data<ArrayList<SuperHero>>>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
